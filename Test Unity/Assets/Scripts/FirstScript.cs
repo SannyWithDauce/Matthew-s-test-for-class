0817e73748d7e1a029ce5e496f92143adb1269cd
SannyWithDauce <73096469+SannyWithDauce@users.noreply.github.com>
2020-10-21 16:34:23 +0200

    Intoduction to Unity

---
+++ Test Unity/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Welcome MSD4.1B");
+        print("to PCG");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Hello everyone");
+    }
+}
